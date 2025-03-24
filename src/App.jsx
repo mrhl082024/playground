2b57299da407e1ef224fa9757f102bb56ca27ac4
mrhl082024 <mrhl082024@gmail.com>
2025-03-24 12:43:21 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import "./App.css";
-
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Chart } from "react-chartjs-2";
+import data from "./data/data.json";
 function App() {
   return (
     <>
-      <div>test</div>
+      <Chart
+        data={{
+          labels: data.map((data) => data.label),
+          datasets: [
+            {
+              type: "doughnut",
+              label: "count",
+              data: data.map((data) => data.value),
+            },
+          ],
+        }}
+      />
     </>
   );
 }
